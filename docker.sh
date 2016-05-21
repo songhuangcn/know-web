1b07440d7ad788507f3551f8640f2e48de76a0e9
Song Huang <songhuangcn@qq.com>
2016-05-21 22:46:50 +0800

    init

--- docker.sh
+++ docker.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-echo '/*** Gem源:'
-gem update --system
-gem sources -r https://rubygems.org/ -a https://gems.ruby-china.org/;
-gem install bundle;
-bundle config mirror.https://rubygems.org https://gems.ruby-china.org;
-bundle config mirror.http://rubygems.org https://gems.ruby-china.org;
-bundle config mirror.https://ruby.taobao.org https://gems.ruby-china.org;
-bundle config mirror.http://ruby.taobao.org https://gems.ruby-china.org;
+# echo '/*** Gem源:'
+# gem update --system
+# gem sources -r https://rubygems.org/ -a https://gems.ruby-china.org/;
+# gem install bundle;
+# bundle config mirror.https://rubygems.org https://gems.ruby-china.org;
+# bundle config mirror.http://rubygems.org https://gems.ruby-china.org;
+# bundle config mirror.https://ruby.taobao.org https://gems.ruby-china.org;
+# bundle config mirror.http://ruby.taobao.org https://gems.ruby-china.org;
 
-echo '/*** Bundle install:'
-bundle install;
+# echo '/*** Bundle install:'
+# bundle install;
 
 echo '/*** Migrate:'
 rake db:create;
