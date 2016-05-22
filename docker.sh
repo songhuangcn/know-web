c7ddeb75befea0d1f7e162ed5911e0ff566344db
PineWong <PineWong@163.com>
2016-05-22 20:34:10 +0800

    init

--- docker.sh
+++ docker.sh
@@ -8,11 +8,21 @@
 # bundle config mirror.http://rubygems.org https://gems.ruby-china.org;
 # bundle config mirror.https://ruby.taobao.org https://gems.ruby-china.org;
 # bundle config mirror.http://ruby.taobao.org https://gems.ruby-china.org;
+# Bundle
+
+# RUN gem update --system
+echo '/*** Bundle Install:';
+gem sources -r https://rubygems.org/ -a https://gems.ruby-china.org/;
+gem source;
+gem install bundler;
+bundle config mirror.https://rubygems.org https://gems.ruby-china.org;
+bundle install;
+rake assets:precompile;
 
 # echo '/*** Bundle install:'
 # bundle install;
 
-echo '/*** Migrate:'
+echo '/*** Rake Db:';
 rake db:create;
 rake db:migrate;
 
