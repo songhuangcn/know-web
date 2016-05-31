9ea1c12ecdbabaf555b563848e939c9d1abec60b
Song Huang <songhuangcn@qq.com>
2016-05-31 20:56:43 +0800

    aaa

---
+++ public/assets/download-e087579ada28d87e9f9d1250725ceee9da17017e981440ae7ae9b24f32472b5d.js
@@ -0,0 +1,54 @@
+/**
+ * 
+ * @authors Pine Wong (pinewong@163.com)
+ * @date    2016-05-13 22:19:53
+ * @version $Id$
+ */
+
+
+$(function() {
+	// download-icon图标变化
+	downloadIconChange();
+
+	// 更换下载内容
+	changeDownloadCategory();
+});
+
+
+
+
+// download-icon图标变化
+function downloadIconChange() {
+	var arr = $('#downloads-content .windows');
+	$.each(arr, function() {
+		$(this).mouseover(function(key, value) {
+		alert($(this));
+			alert($(this) + ': '+key + ': '+value);
+		});	
+	});
+}
+
+function changeDownloadCategory() {
+	$('#downloads_content select').change(function() {
+		var selectCategory = $('#downloads_content select').val();
+		// alert(selectCategory);
+		if (selectCategory == 'ides') {
+			$('#down-ides').fadeIn();
+			$('#down-tools').fadeOut();
+			$('#down-editors').fadeOut();
+		} else if (selectCategory == 'tools') {
+			$('#down-ides').fadeOut();
+			$('#down-tools').fadeIn();
+			$('#down-editors').fadeOut();
+		}  else if (selectCategory == 'editors'){
+			$('#down-ides').fadeOut();
+			$('#down-tools').fadeOut();
+			$('#down-editors').fadeIn();
+		}
+	});
+
+	$('#banner').mouseover(function() {
+		clearInterval(slide);
+		$('#banner').animate({ opacity: 0.8 });
+	});
+}
---
+++ public/assets/topics-caf83c26119f4f48642a6afc48e222ec2471c4b9a0e8cb30f8a12e652a1f718d.js
@@ -0,0 +1,8 @@
+/**
+ * 
+ * @authors Pine Wong (pinewong@163.com)
+ * @date    2016-05-25 20:03:22
+ * @version $Id$
+ */
+
+
