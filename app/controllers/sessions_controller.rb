ae2873840ff755e9ac84798a5ea984c665a4a45c
PineWong <PineWong@163.com>
2016-06-01 09:05:53 +0800

    re link

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,8 @@ class SessionsController < ApplicationController
     if user && !!user.authenticate(params[:session][:password])
     	log_in user
       current_user.update(score: current_user.score+score(:login))
-    	redirect_back_or user and return
+      flash[:success] = "登陆成功，积分+ #{score(:login)}"
+   	  redirect_back_or user and return
     else
       flash.now[:danger] = '登陆失败，账户名或密码错误'
       render 'new' and return
@@ -17,6 +18,7 @@ class SessionsController < ApplicationController
 
   def destroy
   	log_out
+    flash[:success] = "注销成功，每次登陆会增加 #{score(:login)} 积分，记得每天登陆哦"
   	redirect_to :back
   end
 end
